6705259b78105e87963e4f6da51ba6d427319f0e
Jens_Nilsen <jensnilsen89@gmail.com>
2020-04-15 16:29:46 +0200

    login endpoint added

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ import mongoose from 'mongoose'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt-nodejs'
 
-const port = process.env.PORT || 8080
-const app = express()
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/undefbackend"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
@@ -29,6 +26,10 @@ const User = mongoose.model('User', {
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString('hex')
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 })
 
@@ -48,13 +49,46 @@ const Admin = mongoose.model('Admin', {
   }
 })
 
+// const Chatroom= mongoose.model('Chatroom', {
+//   chatname: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   accessToken: {
+//     type: String,
+//     default: () => crypto.randomBytes(128).toString('hex')
+//   }
+// })
+
+// const Message = mongoose.model('Message', {
+//   message: {
+//     type: String
+//   }
+// })
+
+
+const port = process.env.PORT || 8080
+const app = express()
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Undefinedsssss')
+  try {
+    console.log("/")
+    res.send('Undefined backend')
+  } catch (err) {
+    res
+      .status(400)
+      .json({ messsage: 'error', error: err.errors })
+  }
 })
 
 // User Signup endpoint
@@ -67,6 +101,7 @@ app.post('/createuser', async (req, res) => {
       password: bcrypt.hashSync(password)
     })
     newUser.save()
+    console.log('creating user:', username)
     res.status(201).json(newUser)
   } catch (err) {
     res
@@ -75,7 +110,7 @@ app.post('/createuser', async (req, res) => {
   }
 })
 
-//create admin
+// //create admin
 app.post('/createadmin', async (req, res) => {
   try {
     const { adminname, password } = req.body
@@ -84,11 +119,65 @@ app.post('/createadmin', async (req, res) => {
       password: bcrypt.hashSync(password)
     })
     newAdmin.save()
+    console.log('creating admin:', adminname)
     res.status(201).json(newAdmin)
   } catch (err) {
     res
       .status(400)
       .json({ messsage: 'Could not create Admin', error: err.errors })
+  }
+})
+
+// find all users
+app.get('/findusers', async (req, res) => {
+  console.log('fetching users')
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ messsage: 'Could not find any users', error: err.errors })
+  }
+})
+
+// Rout for user logging in
+app.post('/userlogin', async (req, res) => {
+  console.log(req.body)
+  const user = await User.findOne({ username: req.body.username })
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({
+      username: user.username,
+      userId: user._id,
+      accessToken: user.accessToken,
+      message: 'yaye ure in'
+    })
+  } else {
+    res.status(401).json({
+      statusCode: 401,
+      notFound: true,
+      error: 'Login failed, username or password incorrect'
+    })
+  }
+})
+
+// Rout for admin logging in
+app.post('/adminlogin', async (req, res) => {
+  console.log(req.body)
+  const admin = await Admin.findOne({ adminname: req.body.adminname })
+  if (admin && bcrypt.compareSync(req.body.password, admin.password)) {
+    res.json({
+      adminname: admin.adminname,
+      adminId: admin._id,
+      accessToken: admin.accessToken,
+      message: 'yaye ure in'
+    })
+  } else {
+    res.status(401).json({
+      statusCode: 401,
+      notFound: true,
+      error: 'Login failed, admin or password incorrect'
+    })
   }
 })
 
