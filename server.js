b8489a6952ac7667977ff736df9e44d67264fe19
Jens_Nilsen <jensnilsen89@gmail.com>
2020-04-15 12:37:17 +0200

    mongo setup, user and admin models added with signup endpoints

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/undefbackend"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const Admin = mongoose.model('Admin', {
+  adminname: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -15,7 +54,42 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Undefinedsssss')
+})
+
+// User Signup endpoint
+app.post('/createuser', async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+    const newUser = await new User({
+      username,
+      email,
+      password: bcrypt.hashSync(password)
+    })
+    newUser.save()
+    res.status(201).json(newUser)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ messsage: 'Could not create user', error: err.errors })
+  }
+})
+
+//create admin
+app.post('/createadmin', async (req, res) => {
+  try {
+    const { adminname, password } = req.body
+    const newAdmin = await new Admin({
+      adminname,
+      password: bcrypt.hashSync(password)
+    })
+    newAdmin.save()
+    res.status(201).json(newAdmin)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ messsage: 'Could not create Admin', error: err.errors })
+  }
 })
 
 // Start the server
