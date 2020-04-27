23f4be267e22b73a13fdf7732e92fd10ebc84060
Erik Pantzar <erik@epantzar.se>
2020-04-27 12:20:10 +0200

    Fix typo (#13)

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,19 +1,19 @@
-import { User } from '../models/user'
+import { User } from "../models/User"
 
 const auth = async (req, res, next) => {
   try {
-    const bearerToken = req.header('Authorization')
+    const bearerToken = req.header("Authorization")
     if (!bearerToken) {
-      throw new Error('no access token provided')
+      throw new Error("no access token provided")
     }
-    const accessToken = bearerToken.split(' ')[1]
+    const accessToken = bearerToken.split(" ")[1]
 
     const hasToken = await User.findOne({ accessToken })
     if (hasToken) {
       next()
     } else {
       // throw new createError(403, 'you are not authorized to access this') // TODO fix status code in error handling
-      throw new Error('you are not authorized')
+      throw new Error("you are not authorized")
     }
   } catch (err) {
     next(err)
