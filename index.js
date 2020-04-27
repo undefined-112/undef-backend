29b3d51482e0a60e17053bf17dbfc14118a270ed
Erik Pantzar <erik@epantzar.se>
2020-04-27 11:59:46 +0200

    Update index

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-import http from "http"
-import https from "https"
-import fs from "fs"
-
 import app from "./src/App"
-
-const privateKey = fs.readFileSync("./server.key", "utf8")
-const serverCert = fs.readFileSync("./server.cert", "utf8")
 
 const port = process.env.PORT || 8080
 
-const httpServer = http.createServer(app)
-
-const httpsServer = https.createServer(
-  {
-    key: privateKey,
-    cert: serverCert,
-  },
-  app
-)
-
-httpServer.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
-
-httpsServer.listen(port + 1, () => {
-  console.log(`Server running on https://localhost:${port + 1}`)
+app.listen(port, () => {
+  console.log(`Backend running on http://localhost:${port}`)
 })
