a2c60faa558792646688b5a1bd8e93e960043d90
Viktor Dahl <tehvicke@gmail.com>
2020-05-09 14:30:29 +0200

    Route testing (#15)
    * moved the db connection out from App.js to index.js to allow for database setup for tests
    
    * added test infra and first test to validate server is started
    
    * added tests for register endpoint

---
+++ src/routes/__tests__/register.test.js
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import 'babel-polyfill' /* For some reason needed for async/await testing */
+import app from '../../app.js'
+
+/* Need to access the database to test like functionality */
+import mongoose from 'mongoose'
+import User from '../../models/User.js'
+const mongoUrl = 'mongodb://localhost/happyThoughtsTest'
+
+let server
+const PORT = 3001
+
+beforeAll(async () => {
+  mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.Promise = Promise
+  await User.deleteMany({})
+  server = app.listen(PORT)
+})
+
+afterAll((done) => {
+  mongoose.connection.close()
+  server.close(done)
+})
+
+describe('register testing', () => {
+  it('registers a user', async () => {
+    const payload = { username: 'user', password: 'password' }
+    await request(server).post('/api/register/').send(payload).expect(201)
+  }),
+    it('does not register a user missing username or password', async () => {
+      const payload1 = { username: 'user' }
+      const payload2 = { password: 'password' }
+      await request(server).post('/api/register/').send(payload1).expect(500)
+      await request(server).post('/api/register/').send(payload2).expect(500)
+    })
+})
