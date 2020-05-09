699d0041b25f9d14e8ab814d8227833038501139
Viktor Dahl <tehvicke@gmail.com>
2020-05-09 12:08:35 +0200

    moved the db connection out from App.js to index.js to allow for database setup for tests

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import express from "express"
-import bodyParser from "body-parser"
-import cors from "cors"
-import mongoose from "mongoose"
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
-import routes from "./routes"
-import auth from "./helpers/auth"
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/undefbackend"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+import routes from './routes'
+import auth from './helpers/auth'
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get("/api/", (req, res) => {
+app.get('/api/', (req, res) => {
   try {
-    res.send("Undefined backend")
+    res.send('Undefined backend')
   } catch (err) {
-    res.status(400).json({ message: "error", error: err.errors })
+    res.status(400).json({ message: 'error', error: err.errors })
   }
 })
 /* Unprotected routes below */
-app.use("/api", routes.register)
-app.use("/api", routes.login)
+app.use('/api', routes.register)
+app.use('/api', routes.login)
 
 /* Protected routes below */
 app.use(auth)
-app.use("/api", routes.chats)
+app.use('/api', routes.chats)
 
 /* */
 app.use((req, res) => {
