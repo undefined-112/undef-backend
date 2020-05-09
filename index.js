34051fee85fbae5ebafd32931efcd26da724150b
Viktor Dahl <tehvicke@gmail.com>
2020-05-09 14:21:12 +0200

    Unittesting (#14)
    * moved the db connection out from App.js to index.js to allow for database setup for tests
    
    * added test infra and first test to validate server is started

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import app from "./src/App"
+import app from './src/App'
+import mongoose from 'mongoose'
 
 const port = process.env.PORT || 8080
 
-app.listen(port, () => {
-  console.log(`Backend running on http://localhost:${port}`)
-})
+/* Connect to the database */
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1/undefbackend'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+/* Start the server */
+app.listen(port, () =>
+  console.log(
+    `Backend running on http://127.0.0.1:${port}, connected to db mongodb://${mongoose.connection.host}:${mongoose.connection.port}/${mongoose.connection.name}`
+  )
+)
