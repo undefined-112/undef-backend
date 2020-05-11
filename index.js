740a269f8eaf2be5623635701d70ce19d7d6c87a
Viktor Dahl <tehvicke@gmail.com>
2020-05-11 11:21:46 +0200

    restored index.js

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const port = process.env.PORT || 8080
 
 /* Connect to the database */
 const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1/undefbackend'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
-mongoose.set('useCreateIndex', true)
 
 /* Start the server */
 app.listen(port, () =>
   console.log(
-    `Backend running on http://127.0.0.1:${port}, connected to db mongodb://${mongoose.connection.host}:${mongoose.connection.port}/${mongoose.connection.name}`,
-  ),
+    `Backend running on http://127.0.0.1:${port}, connected to db mongodb://${mongoose.connection.host}:${mongoose.connection.port}/${mongoose.connection.name}`
+  )
 )
